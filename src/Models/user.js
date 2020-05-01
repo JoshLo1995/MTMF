483075db6fe95c95299375180fef3dfbc64d349c
Joshua Lo <josh.lol1995@gmail.com>
2020-04-30 21:44:44 -0700

    fleshed out the user model

--- src/Models/user.js
+++ src/Models/user.js
@@ -5,35 +5,45 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
     {
-        username: {
-            type: String,
-            required: "Enter a username to login"
-        },
-        password: {
-            type: String,
-            required: "Enter a password to login"
-        }
+        user: {
+            username: {
+                type: String,
+                required: "Enter a username to login"
+            },
+            password: {
+                type: String,
+                required: "Enter a password to login"
+            }
+        }   
     }
 );
 const User = mongoose.model('User', userSchema);
     
+// Call from sign up page
 User.prototype.setUserName = () => {
-    let username;
+    let username = `${insertusernamefieldhere}`;
 
     return username;
 }
 
-User.prototype.setPassword = () => {
-    bcrypt.hash(`${insertpasswordfieldhere}`, 10, (err, hash) => {
-        // Store hash in database
-        if (err) throw err;
-    })
-}
-
+// Call from sign up page
 User.prototype.createNewUser = () => {
     let username = setUserName();
-    // create query to send username to db
-    setPassword();
+    bcrypt.hash(`${insertpasswordfieldhere}`, 10, (err, hash) => {
+        // Store hash in database
+        if (hash) {
+            this.db.MTMFdb.insertOne(
+                {
+                    user: {
+                        username: username,
+                        password: hash
+                    }
+                }
+            )
+        } else {
+            throw err;
+        }
+    })
 }
 
 User.prototype.verifyPassword = () => {
